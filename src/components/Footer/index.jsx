d98aa7a33396b60ecefe02419783b9f07dd9d2bd
Maria Amigo <maria.amigo.07@gmail.com>
2024-07-24 19:50:11 +0200

    ajout du composant Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import logo from '../../assets/logo_white.svg'
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <img scr={logo} alt="Logo de Kasa en blanc" />
+        <p>© 2020 Kasa. All rights reserved</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
