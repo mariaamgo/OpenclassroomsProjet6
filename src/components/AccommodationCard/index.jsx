c93774d900d042d41eace0e5da0ad53db2a5f4f6
Maria Amigo <maria.amigo.07@gmail.com>
2024-08-27 07:53:49 +0200

    ajout du composant AccommodationCard

---
+++ src/components/AccommodationCard/index.jsx
@@ -0,0 +1,51 @@
+import pinkStar from '../../assets/pink_star.svg'
+import greyStar from '../../assets/grey_star.svg'
+
+function AccommodationCard({ announcement }) {
+  const [firstName, lastName] = announcement.host.name.split(' ')
+
+  const numbers = [1, 2, 3, 4, 5]
+
+  return (
+    <div className="accomodation">
+      <div className="accomodation__details">
+        <h1>{announcement.title}</h1>
+        <p>{announcement.location}</p>
+        <ul>
+          {announcement.tags.map((tag, index) => (
+            <li key={`tag-${index}`}>{tag}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="accomodation__info">
+        <div className="accomodation__host">
+          <p>
+            {firstName}
+            <br />
+            {lastName}
+          </p>
+          <img src={announcement.host.picture} alt="" />
+        </div>
+        <div className="accomodation__rating">
+          {numbers.map((number) =>
+            announcement.rating >= number ? (
+              <img
+                src={pinkStar}
+                alt="Étoile rose"
+                key={`star-${number.toString()}`}
+              />
+            ) : (
+              <img
+                src={greyStar}
+                alt="Étoile grise"
+                key={`star-${number.toString()}`}
+              />
+            ),
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AccommodationCard
