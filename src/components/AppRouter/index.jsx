b8a506ad0c52788d5c897d0e1307c1157fcc4bfc
Maria Amigo <maria.amigo.07@gmail.com>
2024-08-27 07:54:40 +0200

    modification du composants AppRouter

--- src/components/AppRouter/index.jsx
+++ src/components/AppRouter/index.jsx
@@ -2,25 +2,26 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from '../Header'
 import Footer from '../Footer'
-import Error from '../Error'
+import Error from '../../pages/Error'
 import Home from '../../pages/Home'
-import AccommodationListing from '../../pages/AccommodationListing'
+import Accommodation from '../../pages/Accommodation'
 import About from '../../pages/About'
 
 function AppRouter() {
   return (
     <Router>
       <Header />
-      <Footer />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route
-          path="/accommodation_listing"
-          element={<AccommodationListing />}
-        />
         <Route path="/a_propos" element={<About />} />
+        <Route
+          path="/accommodation/:accomodationId"
+          element={<Accommodation />}
+        />
+        <Route path="/error" element={<Error />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <Footer />
     </Router>
   )
 }
