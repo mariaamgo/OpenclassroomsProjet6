3f75a41655fb65cdddfa01c9497008cb076d2efe
Maria Amigo <maria.amigo.07@gmail.com>
2024-08-27 08:04:56 +0200

    ajout de la page Accommodation

---
+++ src/pages/Accommodation/index.jsx
@@ -0,0 +1,59 @@
+import Collapse from '../../components/Collapse'
+import Slideshow from '../../components/Slideshow'
+import AccommodationCard from '../../components/AccommodationCard'
+import announcementList from '../../datas/announcementList.json'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+
+function AccommodationListing() {
+  const { accomodationId } = useParams()
+
+  const announcement = announcementList.find(
+    (announcement) => announcement.id === accomodationId,
+  )
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!announcement) {
+      // Redirection vers la page d'erreur si l'ID est invalide
+      navigate('/error', { replace: true })
+    }
+  }, [announcement]) // Dépendances pour garantir que l'effet se déclenche au bon moment
+
+  // Si `announcement` est `undefined`, on peut aussi retourner `null` pour éviter un rendu incomplet
+  if (!announcement) {
+    return null // ou un indicateur de chargement, selon ce que tu préfères
+  }
+
+  const equipments = (
+    <ul>
+      {announcement.equipments.map((equipment, index) => (
+        <li key={`equipment-${index}`} className="collapse__list--equipment">
+          {equipment}
+        </li>
+      ))}
+    </ul>
+  )
+
+  return (
+    <>
+      <Slideshow pictures={announcement.pictures} />
+      <AccommodationCard announcement={announcement} />
+      <ul className="collapse collapse--accomodation">
+        <Collapse
+          id={`description-${announcement.id}`}
+          title="Description"
+          description={announcement.description}
+        />
+        <Collapse
+          id={`equipments-${announcement.id}`}
+          title="Équipement"
+          description={equipments}
+        />
+      </ul>
+    </>
+  )
+}
+
+export default AccommodationListing
