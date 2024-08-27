608b3cd5e2b0325de01fc31cb3ddfe6d14841b11
Maria Amigo <maria.amigo.07@gmail.com>
2024-08-27 07:59:30 +0200

    ajout du composant Slideshow

---
+++ src/components/Slideshow/index.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import arrowRight from '../../assets/arrow_right.svg'
+import arrowLeft from '../../assets/arrow_left.svg'
+
+function Slideshow({ pictures }) {
+  const [index, setIndex] = useState(0)
+
+  const length = pictures.length
+
+  const previousImage = () => {
+    const newIndex = index - 1
+    setIndex(newIndex < 0 ? length - 1 : newIndex)
+  }
+
+  const nextImage = () => {
+    const newIndex = index + 1
+    setIndex(newIndex >= length ? 0 : newIndex)
+  }
+
+  return (
+    <div className="carousel">
+      <img src={pictures[index]} alt="" className="carousel__image" />
+      {pictures.length > 1 && (
+        <>
+          <button onClick={previousImage} className="carousel__button--left">
+            <img src={arrowLeft} alt="" />
+          </button>
+          <button onClick={nextImage} className="carousel__button--right">
+            <img src={arrowRight} alt="" />
+          </button>
+          <p className="carousel__number">
+            {index + 1}/{length}
+          </p>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default Slideshow
