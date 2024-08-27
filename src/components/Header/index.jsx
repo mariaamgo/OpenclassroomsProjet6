6de0e57879d2bfba0e198f1ef0af37b50609befd
Maria Amigo <maria.amigo.07@gmail.com>
2024-08-27 07:57:57 +0200

    modification du composant Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,21 +4,27 @@ import logo from '../../assets/logo_pink.svg'
 function Header() {
   return (
     <header>
-      <img src={logo} alt="Logo de Kasa" />
-      <nav>
-        <NavLink
-          to="/"
-          className={({ isActive }) => (isActive ? 'link active-link' : 'link')}
-          end
-        >
-          Accueil
-        </NavLink>
-        <NavLink
-          to="/a_propos"
-          className={({ isActive }) => (isActive ? 'link active-link' : 'link')}
-        >
-          A Propos
-        </NavLink>
+      <nav className="navbar">
+        <img src={logo} alt="Logo de Kasa" className="navbar__image" />
+        <div>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? 'navbar__link navbar__link--active' : 'navbar__link'
+            }
+            end
+          >
+            Accueil
+          </NavLink>
+          <NavLink
+            to="/a_propos"
+            className={({ isActive }) =>
+              isActive ? 'navbar__link navbar__link--active' : 'navbar__link'
+            }
+          >
+            A Propos
+          </NavLink>
+        </div>
       </nav>
     </header>
   )
