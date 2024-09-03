acc5707dc748548d487fee3bb5f273d7674c47b1
Maria Amigo <maria.amigo.07@gmail.com>
2024-09-03 06:46:35 +0200

    retrait de class (pour éviter d'en avoir trop)

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,8 +4,8 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer__content">
-        <img src={logo} alt="Logo de Kasa en blanc" className="footer__logo" />
-        <p className="footer__text">© 2020 Kasa. All rights reserved</p>
+        <img src={logo} alt="Logo de Kasa en blanc" />
+        <p>© 2020 Kasa. All rights reserved</p>
       </div>
     </footer>
   )
