0e8b46fc4fc37c4e7c983a1280ce8654688e454c
Maria Amigo <maria.amigo.07@gmail.com>
2024-09-09 06:46:52 +0200

    Modification de la ligne d'import

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import Arrow from '../../assets/arrow.svg'
 
 function Collapse({ id, title, description }) {
